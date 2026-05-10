ead22a91cfad26e00a1445acd1241fce81db3bfc
Joey Hain <59634053+Joey-Hain@users.noreply.github.com>
2026-05-10 16:02:52 +1000

    Add files via upload
    see prev commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
@@ -37,4 +38,5 @@ def predict():
     return jsonify({"fatigue_score": round(float(score), 1)})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
